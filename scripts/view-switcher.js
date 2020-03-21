6eaf9d6726b63291761d6c00017a1d1cf98e1b5c
Chaphasilor <ppp.u@web.de>
2020-03-21 02:08:31 +0100

    added ui-based navigation

--- scripts/view-switcher.js
+++ scripts/view-switcher.js
@@ -2,33 +2,53 @@ class ViewSwitcher {
 
   constructor(marketView, cartView, mapView) {
 
-    this.marketView = marketView;
-    this.cartView = cartView;
-    this.mapView = mapView;
+    this.marketView = document.querySelector(`#${marketView}`);
+    this.marketViewNav = document.querySelector(`#nav-${marketView}`);
+    this.cartView = document.querySelector(`#${cartView}`);
+    this.cartViewNav = document.querySelector(`#nav-${cartView}`);
+    this.mapView = document.querySelector(`#${mapView}`);
+    this.mapViewNav = document.querySelector(`#nav-${mapView}`);
     
   }
 
   showMarkets() {
 
     this.marketView.classList.add('visible');
+    this.marketViewNav.classList.add('active');
+
     this.cartView.classList.remove('visible');
+    this.cartViewNav.classList.remove('active');
+
     this.mapView.classList.remove('visible');
+    this.mapViewNav.classList.remove('active');
     
   }
 
   showCart() {
 
     this.marketView.classList.remove('visible');
+    this.marketViewNav.classList.remove('active');
+
     this.cartView.classList.add('visible');
+    this.cartViewNav.classList.add('active');
+
     this.mapView.classList.remove('visible');
+    this.mapViewNav.classList.remove('active');
+
     
   }
 
   showMap() {
 
     this.marketView.classList.remove('visible');
+    this.marketViewNav.classList.remove('active');
+    
     this.cartView.classList.remove('visible');
+    this.cartViewNav.classList.remove('active');
+    
     this.mapView.classList.add('visible');
+    this.mapViewNav.classList.add('active');
+    
     
   }
   
