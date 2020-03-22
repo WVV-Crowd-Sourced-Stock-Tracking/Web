62e1649d3de36188ef0a6d6e03e39f6365520d74
Chaphasilor <ppp.u@web.de>
2020-03-22 15:00:24 +0100

    updated mock db
    - also made map marker non-draggable

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -16,7 +16,7 @@
           v-for="(m, index) in markers"
           :position="m.position"
           :clickable="true"
-          :draggable="true"
+          :draggable="false"
           @click="center = m.position"
         />
       </GmapMap>
