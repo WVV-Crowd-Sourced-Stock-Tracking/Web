69fab3354c25bc029858c0662381294dcacd1465
Veit Progl <mail@veit.dev>
2020-03-22 20:15:37 +0100

    shop detail view style

--- src/views/MarketDetail.vue
+++ src/views/MarketDetail.vue
@@ -5,28 +5,43 @@
     </div>
 
     <div class="main">
-      <div class="address">
-        <h1 class="text-m font-semibold">Addresse:</h1>
-        {{ this.$route.params.address }}
+      <div class="zwei-spalten">
+        <div class="address">
+          <h1 class="text-m font-semibold">Addresse:</h1>
+          {{ this.$route.params.address }}
+        </div>
+        <div>
+          <h1 class="text-m font-semibold">Öffnungszeiten:</h1>
+          <div class="status">
+            <span v-bind:class="this.$route.params.status.class">{{
+              this.$route.params.status.text
+            }}</span>
+          </div>
+        </div>
       </div>
-      <h1 class="text-m font-semibold">Öffnungszeiten:</h1>
-      <div class="status">
-        <span v-bind:class="this.$route.params.status.class">{{
-          this.$route.params.status.text
-        }}</span>
-      </div>
+      <div class="map w-full"> 
 
-      <table class="table-auto">
+      </div>
+      
+
+      <table class="table-auto w-full">
         <thead>
           <tr>
-            <th class="px-4 py-2"></th>
             <th class="px-4 py-2">Produkte</th>
-            <th class="px-4 py-2">Bestand</th>
+            <th class="px-4 py-2">Verfügbarkeit</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="item in this.$route.params.products" :key="item.name">
+            <td class="border px-4 py-2">{{ item.name }}</td>
             <td class="border px-4 py-2">
+              <div class="verfugbarkeit-item">
+                <span v-if="item.availability === 'high'">vorrätig</span>
+              <span v-else-if="item.availability === 'medium'"
+                >fast ausverkauft</span
+              >
+              <span v-else>ausverkauft</span>
+
               <div
                 class="h-5 w-5 rounded-full bg-green-500"
                 v-if="item.availability === 'high'"
@@ -36,14 +51,7 @@
                 v-else-if="item.availability === 'medium'"
               ></div>
               <div class="h-5 w-5 rounded-full bg-red-500" v-else></div>
-            </td>
-            <td class="border px-4 py-2">{{ item.name }}</td>
-            <td class="border px-4 py-2">
-              <span v-if="item.availability === 'high'">vorrätig</span>
-              <span v-else-if="item.availability === 'medium'"
-                >fast ausverkauft</span
-              >
-              <span v-else>ausverkauft</span>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -54,111 +62,104 @@
   </div>
 </template>
 
-<style scoped>
-.card {
-  display: block;
-  position: relative;
-  margin-top: 2%;
-  width: 100%;
-  height: 100vh;
-  border-radius: 0.5rem;
-  overflow: hidden;
-}
+<style lang="sass">
+.zwei-spalten
+  display: grid
+  grid-template-columns: 1fr 1fr
+.map
+  height: 300px
+  background: black
 
-.card .header {
-  display: block;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: #006bab;
-  padding-left: 1rem;
-  color: white;
-}
+.verfugbarkeit-item 
+  display: flex
+  align-items: center
+  justify-content: center
 
-.card .header h2 {
-  display: inline-block;
-  position: relative;
-  top: 0;
-  width: 60%;
-  height: 100%;
-  font-size: calc(var(--header-height) / 2);
-  font-weight: bold;
-  line-height: 2.5rem;
-}
+  * 
+    padding-right: 10px
 
-.card .header img {
-  display: inline-block;
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: calc(var(--header-height));
-  height: calc(var(--header-height));
-  filter: invert(1);
-}
+.card 
+  display: block
+  position: relative
+  margin-top: 2%
+  width: 100%
+  border-radius: 0.5rem
+  overflow: hidden
 
-.card .main {
-  position: absolute;
-  top: var(--header-height);
-  width: 100%;
-  height: calc(100% - var(--header-height));
-  padding: 3%;
-  background-color: white;
-  line-height: 2rem;
-}
+.card .header 
+  display: block
+  width: 100%
+  height: auto
+  background-color: #006bab
+  padding-left: 1rem
+  color: white
 
-.card .main .address {
-  display: inline-block;
-}
+.card .header h2 
+  display: inline-block
+  position: relative
+  top: 0
+  height: 100%
+  font-size: calc(var(--header-height) / 2)
+  font-weight: bold
+  line-height: 2.5rem
 
-.card .main .distance {
-  display: inline-block;
-  position: absolute;
-  right: 2rem;
-}
+.card .header img 
+  display: inline-block
+  position: absolute
+  right: 0
+  top: 0
+  width: calc(var(--header-height))
+  height: calc(var(--header-height))
+  filter: invert(1)
 
-.card .main .status span {
-  font-weight: bold;
-}
+.card .main 
+  width: 100%
+  padding: 3%
+  background-color: white
+  line-height: 2rem
 
-.card .main .status span.opened {
-  color: limegreen;
-}
+.card .main .address 
+  display: inline-block
 
-.card .main .status span.closed {
-  color: crimson;
-}
+.card .main .distance 
+  display: inline-block
+  position: absolute
+  right: 2rem
 
-.card .main .categories {
-  display: block;
-  width: 100%;
-  margin-top: 1rem;
-}
+.card .main .status span 
+  font-weight: bold
 
-.card .main .categories ul {
-  display: flex;
-  flex-direction: right;
-}
+.card .main .status span.opened 
+  color: limegreen
 
-.card .main .categories ul li .traffic-light {
-  display: inline-block;
-  width: 1rem;
-  height: 1rem;
-  border-radius: 100%;
-}
+.card .main .status span.closed 
+  color: crimson
 
-.card .main .categories ul li .label {
-  margin-left: 0.5rem;
-}
+.card .main .categories 
+  display: block
+  width: 100%
+  margin-top: 1rem
 
-.card .main .categories ul li {
-  margin-right: 2rem;
-}
+.card .main .categories ul 
+  display: flex
+  flex-direction: right
 
-.card .main .updated {
-  width: 100%;
-  text-align: center;
-  margin-top: 0.5rem;
-  color: #c8cbcd;
-}
+.card .main .categories ul li .traffic-light 
+  display: inline-block
+  width: 1rem
+  height: 1rem
+  border-radius: 100%
+
+.card .main .categories ul li .label 
+  margin-left: 0.5rem
+
+.card .main .categories ul li 
+  margin-right: 2rem
+
+.card .main .updated 
+  width: 100%
+  text-align: center
+  margin-top: 0.5rem
+  color: #c8cbcd
+
 </style>
